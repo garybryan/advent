e59d70611ce25747ea4ce529909baab0369ef6e8
Gary Bryan <garybryan@gmail.com>
2022-11-30 22:41:23 +0000

    2021 day 2 part 1

---
+++ 2021/2.hs
@@ -0,0 +1,26 @@
+import Runner
+
+type Command = (String, Int)
+type Vector = (Int, Int)
+
+parseLine :: String -> Command
+parseLine line = (head splitLine, read (last splitLine))
+  where splitLine = words line
+
+getCommandVector :: Command -> Vector
+getCommandVector ("up", n)      = (-n, 0)
+getCommandVector ("down", n)    = (n, 0)
+getCommandVector ("forward", n) = (0, n)
+
+addVectors :: Vector -> Vector -> Vector
+addVectors a b = (fst a + fst b, snd a + snd b)
+
+getFinalPosition :: [Command] -> Vector
+getFinalPosition cs = foldr addVectors (0, 0) (map getCommandVector cs)
+
+getArea :: Vector -> Int
+getArea (x, y) = x * y
+
+main = do
+  result <- run (show . getArea . getFinalPosition . map parseLine)
+  putStrLn result
