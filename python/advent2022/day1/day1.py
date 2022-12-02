67baa37be15ee4098632d601192e082a8f86e07a
Gary Bryan <garybryan@gmail.com>
2022-12-02 19:57:22 +0000

    Day 1 part 2 python

--- python/advent2022/day1/day1.py
+++ python/advent2022/day1/day1.py
@@ -1,3 +1,5 @@
+import heapq
+from itertools import islice
 from typing import Iterable, Iterator
 
 
@@ -18,9 +20,27 @@ def max_calories(lines: Iterable[str]) -> int:
     return max(calories_per_elf(lines))
 
 
+def k_max(k: int, nums: Iterator[int]) -> list[int]:
+    heap = [next(nums) for _ in range(k)]
+    heapq.heapify(heap)
+
+    for num in nums:
+        if num > heap[0]:
+            heapq.heappushpop(heap, num)
+
+    return heap
+
+
+def sum_calories_k_elves(k: int, lines: Iterable[str]) -> int:
+    return sum(k_max(k, calories_per_elf(lines)))
+
+
 def main() -> None:
     with open("data.txt") as f:
         print("Part 1", max_calories(f))
+
+    with open("data.txt") as f:
+        print("Part 2", sum_calories_k_elves(3, f))
 
 
 if __name__ == "__main__":
--- python/advent2022/day1/test_day1.py
+++ python/advent2022/day1/test_day1.py
@@ -1,5 +1,5 @@
 import pytest
-from day1 import calories_per_elf, max_calories
+from day1 import calories_per_elf, k_max, max_calories, sum_calories_k_elves
 
 
 @pytest.fixture
@@ -24,9 +24,23 @@ def lines():
     )
 
 
-def test_calories_per_elf(lines):
-    assert list(calories_per_elf(lines)) == [6000, 4000, 11000, 24000, 10000]
+@pytest.fixture
+def per_elf():
+    return [6000, 4000, 11000, 24000, 10000]
+
+
+def test_calories_per_elf(lines, per_elf):
+    assert list(calories_per_elf(lines)) == per_elf
 
 
 def test_max_calories(lines):
     assert max_calories(lines) == 24000
+
+
+def test_k_max(per_elf):
+    assert set(k_max(3, per_elf)) == {10000, 11000, 24000}
+    assert set(k_max(2, per_elf)) == {11000, 24000}
+
+
+def test_sum_calories_k_elves(lines):
+    assert sum_calories_k_elves(3, lines) == 45000
