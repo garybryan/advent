9aa5657bb8700da158e304affeb34f96ad18337f
Gary Bryan <garybryan@gmail.com>
2022-12-03 19:44:56 +0000

    Oxygen and CO2 ratings

--- haskell/src/Advent2021/Day3/Part2.hs
+++ haskell/src/Advent2021/Day3/Part2.hs
@@ -3,6 +3,9 @@ module Advent2021.Day3.Part2
     filterOnBit,
     mostCommonInPos,
     filterToMostCommonInPos,
+    filterToLeastCommonInPos,
+    oxygenRating,
+    co2Rating,
   )
 where
 
@@ -14,6 +17,8 @@ import Lib.Read (readLines)
 matchBit :: Int -> Int -> Int -> Bool
 matchBit i v n = n `shiftR` i .&. 1 == v
 
+-- todo use bit i
+
 filterOnBit :: Int -> Int -> [Int] -> [Int]
 filterOnBit i v = filter $ matchBit i v
 
@@ -21,7 +26,20 @@ onesInPos :: Int -> [Int] -> Int
 onesInPos i = length . filterOnBit i 1
 
 mostCommonInPos :: Int -> [Int] -> Int
-mostCommonInPos i xs = fromEnum $ onesInPos i xs >= length xs `div` 2
+mostCommonInPos i xs = fromEnum $ onesInPos i xs >= -(length xs `div` (-2))
 
 filterToMostCommonInPos :: Int -> [Int] -> [Int]
 filterToMostCommonInPos i xs = filterOnBit i (mostCommonInPos i xs) xs
+
+filterToLeastCommonInPos :: Int -> [Int] -> [Int]
+filterToLeastCommonInPos i xs = filterOnBit i (mostCommonInPos i xs `complementBit` 0) xs
+
+filterToOne :: Int -> (Int -> [Int] -> [Int]) -> [Int] -> Int
+filterToOne _ _ [x] = x
+filterToOne i filterFn xs = filterToOne (i - 1) filterFn (filterFn i xs)
+
+oxygenRating :: Int -> [Int] -> Int
+oxygenRating numBits = filterToOne (numBits - 1) filterToMostCommonInPos
+
+co2Rating :: Int -> [Int] -> Int
+co2Rating numBits = filterToOne (numBits - 1) filterToLeastCommonInPos
--- haskell/test/Advent2021/Day3/Part2Spec.hs
+++ haskell/test/Advent2021/Day3/Part2Spec.hs
@@ -10,16 +10,30 @@ spec = do
       filterOnBit 1 1 [0, 2, 3, 5, 10] `shouldBe` [2, 3, 10]
 
   describe "mostCommonInPos" $ do
-    it "Gets the most common bit in a given position in a list of numbers" $ do
+    it "gets the most common bit in a given position in a list of numbers" $ do
       mostCommonInPos 0 [1, 2, 3] `shouldBe` 1
       mostCommonInPos 1 [1, 2, 3] `shouldBe` 1
       mostCommonInPos 2 [1, 2, 3, 4] `shouldBe` 0
 
-    it "Gives 1 if 1 and 0 are equally common" $ do
+    it "gives 1 if 1 and 0 are equally common" $ do
       mostCommonInPos 1 [1, 2, 3, 4] `shouldBe` 1
 
   describe "filterToMostCommonInPos" $ do
-    it "Filters numbers to the ones with the most common bit in the given position" $ do
+    it "filters numbers to the ones with the most common bit in the given position" $ do
       filterToMostCommonInPos 0 [1, 2, 3] `shouldBe` [1, 3]
       filterToMostCommonInPos 1 [1, 2, 3, 4] `shouldBe` [2, 3]
       filterToMostCommonInPos 2 [1, 2, 3, 4] `shouldBe` [1, 2, 3]
+
+  describe "filterToLeastCommonInPos" $ do
+    it "filters numbers to the ones with the least common bit in the given position" $ do
+      filterToLeastCommonInPos 0 [1, 2, 3] `shouldBe` [2]
+      filterToLeastCommonInPos 1 [1, 2, 3, 4] `shouldBe` [1, 4]
+      filterToLeastCommonInPos 2 [1, 2, 3, 4] `shouldBe` [4]
+
+  describe "oxygenRating" $ do
+    it "Fiters by the most common bit value starting at MSB until one number is left" $ do
+      oxygenRating 5 [4, 30, 22, 23, 21, 15, 7, 28, 16, 25, 2, 10] `shouldBe` 23
+
+  describe "co2Rating" $ do
+    it "Fiters by the least common bit value starting at MSB until one number is left" $ do
+      co2Rating 5 [4, 30, 22, 23, 21, 15, 7, 28, 16, 25, 2, 10] `shouldBe` 10
