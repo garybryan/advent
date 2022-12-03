62d843d1b430cc2773c07e38a2617d667953b814
Gary Bryan <garybryan@gmail.com>
2022-12-03 14:49:13 +0000

    Bit frequencies.

--- haskell/src/Advent2021/Day3/Base.hs
+++ haskell/src/Advent2021/Day3/Base.hs
@@ -1,4 +1,4 @@
-module Advent2021.Day3.Base (binStrToInt) where
+module Advent2021.Day3.Base (binStrToInt, bitFreqs) where
 
 binCharToInt :: Char -> Int
 binCharToInt c
@@ -8,3 +8,23 @@ binCharToInt c
 
 binStrToInt :: String -> Int
 binStrToInt = foldl1 (\a n -> a * 2 + n) . map binCharToInt
+
+-- Make a blank frequency list for `k` bits.
+initialFreqs :: Int -> [Int]
+initialFreqs k = replicate k 0
+
+-- Get the frequencies of 1 for each bit in a list of numbers.
+-- For each number, it builds a new list by repeatedly dividing the number by two
+-- to get the next bit (the remainder) and adding that to the previous frequency for that position.
+-- Since Haskell works efficiently by concatenating to the head of a list rather than the tail,
+-- and dividing a number by 2 gives its least significant bit, the list is processed in reverse order.
+-- least significant bit first. It's then returned in "normal" order, just to make it easier to reason with.
+-- This is clearly less efficient than it would be using mutability, since it builds a new list each time.
+bitFreqs :: Int -> [Int] -> [Int]
+bitFreqs k = reverse . foldr aux (initialFreqs k)
+  where
+    aux :: Int -> [Int] -> [Int]
+    aux _ [] = []
+    aux x (f : fs) = r + f : aux q fs
+      where
+        (q, r) = x `divMod` 2
--- haskell/test/Advent2021/Day3/BaseSpec.hs
+++ haskell/test/Advent2021/Day3/BaseSpec.hs
@@ -6,6 +6,12 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "binStrToInt" $ do
-    it "converts a string of binary digits representing a positive integer to said integer" $ do
+    it "converts a string of binary digits representing an unsigned integer to said integer" $ do
       binStrToInt "0101" `shouldBe` 5
       binStrToInt "10000000" `shouldBe` 128
+
+  describe "bitFreqs" $ do
+    it "Gets the bit frequencies for a list of integers" $ do
+      bitFreqs 5 [5] `shouldBe` [0, 0, 1, 0, 1]
+      bitFreqs 5 [9, 3, 1] `shouldBe` [0, 1, 0, 1, 3]
+
