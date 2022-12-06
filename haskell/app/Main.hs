47e652fa556e273dc9adfc533eb375eae4fcebc0
Gary Bryan <garybryan@gmail.com>
2022-12-06 09:10:22 +0000

    Data filename leading zeroes

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -23,33 +23,33 @@ import Lib.Run
 main :: IO ()
 main = do
   putStrLn "\n2021 day 1:"
-  runOnFile Advent2021.Day01.run "data/2021/1.txt"
+  runOnFile Advent2021.Day01.run "data/2021/01.txt"
 
   putStrLn "\n2021 day 2:"
-  runOnFile Advent2021.Day02.run "data/2021/2.txt"
+  runOnFile Advent2021.Day02.run "data/2021/02.txt"
 
   putStrLn "\n2021 day 3 part 1:"
-  runOnFile Advent2021.Day03.Part1.run "data/2021/3.txt"
+  runOnFile Advent2021.Day03.Part1.run "data/2021/03.txt"
   putStrLn "\n2021 day 3 part 2:"
-  runOnFile Advent2021.Day03.Part2.run "data/2021/3.txt"
+  runOnFile Advent2021.Day03.Part2.run "data/2021/03.txt"
 
   putStrLn "\n2022 day 1:"
-  runOnFile Advent2022.Day01.run "data/2022/1.txt"
+  runOnFile Advent2022.Day01.run "data/2022/01.txt"
 
   putStrLn "\n2022 day 2 part 1:"
-  runOnFile Advent2022.Day02.Part1.run "data/2022/2.txt"
+  runOnFile Advent2022.Day02.Part1.run "data/2022/02.txt"
   putStrLn "2022 day 2 part 2:"
-  runOnFile Advent2022.Day02.Part2.run "data/2022/2.txt"
+  runOnFile Advent2022.Day02.Part2.run "data/2022/02.txt"
 
   putStrLn "\n2022 day 3 part 1:"
-  runOnFile Advent2022.Day03.Part1.run "data/2022/3.txt"
+  runOnFile Advent2022.Day03.Part1.run "data/2022/03.txt"
   putStrLn "\n2022 day 3 part 2:"
-  runOnFile Advent2022.Day03.Part2.run "data/2022/3.txt"
+  runOnFile Advent2022.Day03.Part2.run "data/2022/03.txt"
 
   putStrLn "\n2022 day 4 part 1:"
-  runOnFile Advent2022.Day04.Part1.run "data/2022/4.txt"
+  runOnFile Advent2022.Day04.Part1.run "data/2022/04.txt"
   putStrLn "\n2022 day 4 part 2:"
-  runOnFile Advent2022.Day04.Part2.run "data/2022/4.txt"
+  runOnFile Advent2022.Day04.Part2.run "data/2022/04.txt"
 
   putStrLn "\n2022 day 5 part 1:"
   runOnFile Advent2022.Day05.Part1.run "data/2022/05.txt"
