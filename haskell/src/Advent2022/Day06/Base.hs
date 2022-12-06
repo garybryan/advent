b0aa490d85e638e7386c36f3218f66c92d0d7dd2
Gary Bryan <garybryan@gmail.com>
2022-12-06 08:50:04 +0000

    Optimisation: set membership check instead of nub for unique prefix
    Takes the k-length prefix check from O(k^2) to O(k log k),
    since Haskell sets seem to be based on trees rather than hashtables.

--- haskell/src/Advent2022/Day06/Base.hs
+++ haskell/src/Advent2022/Day06/Base.hs
@@ -1,17 +1,17 @@
 module Advent2022.Day06.Base (indexAfterUniquePrefix) where
 
-import qualified Data.List as List
+import qualified Data.Set as Set
 
-numUniques :: (Eq a) => [a] -> Int
-numUniques = length . List.nub
+numUniques :: (Ord a) => [a] -> Int
+numUniques = length . Set.fromList
 
-uniquePrefix :: (Eq a) => Int -> [a] -> Bool
+uniquePrefix :: (Ord a) => Int -> [a] -> Bool
 uniquePrefix n xs = n == numUniques (take n xs)
 
-indexAfterUniquePrefix :: (Eq a) => Int -> [a] -> Int
+indexAfterUniquePrefix :: (Ord a) => Int -> [a] -> Int
 indexAfterUniquePrefix n = go 0
   where
-    go :: (Eq a) => Int -> [a] -> Int
+    go :: (Ord a) => Int -> [a] -> Int
     go _ [] = -1
     go i xs'
       | uniquePrefix n xs' = i + n
--- haskell/src/Advent2022/Day06/Part2.hs
+++ haskell/src/Advent2022/Day06/Part2.hs
@@ -2,5 +2,5 @@ module Advent2022.Day06.Part2 (indexAfterMarker) where
 
 import Advent2022.Day06.Base
 
-indexAfterMarker :: (Eq a) => [a] -> Int
+indexAfterMarker :: (Ord a) => [a] -> Int
 indexAfterMarker = indexAfterUniquePrefix 14
