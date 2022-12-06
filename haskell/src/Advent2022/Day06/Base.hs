2230fdf4cde3bb1c0589071f82413d2d15b38957
Gary Bryan <garybryan@gmail.com>
2022-12-06 11:19:54 +0000

    optimisation: don't check prefixes once string length is too short

--- haskell/src/Advent2022/Day06/Base.hs
+++ haskell/src/Advent2022/Day06/Base.hs
@@ -3,6 +3,11 @@ module Advent2022.Day06.Base (indexAfterUniquePrefix, parseLines) where
 import Data.Maybe
 import qualified Data.Set as Set
 
+{-
+  Basic approach: at each index, check if the prefix starting at that index is
+  unique.
+-}
+
 numUniques :: (Ord a) => [a] -> Int
 numUniques = Set.size . Set.fromList
 
@@ -10,11 +15,12 @@ uniquePrefix :: (Ord a) => Int -> [a] -> Bool
 uniquePrefix n xs = n == numUniques (take n xs)
 
 indexAfterUniquePrefix :: (Ord a) => Int -> [a] -> Int
-indexAfterUniquePrefix n = go 0
+indexAfterUniquePrefix n xs = go 0 xs
   where
+    len = length xs
     go :: (Ord a) => Int -> [a] -> Int
-    go _ [] = -1
     go i xs'
+      | i > len - n = -1
       | uniquePrefix n xs' = i + n
       | otherwise = go (i + 1) (tail xs')
 
--- haskell/test/Advent2022/Day06/BaseSpec.hs
+++ haskell/test/Advent2022/Day06/BaseSpec.hs
@@ -9,8 +9,16 @@ spec = do
     it "Finds the end index of the first unique prefix of given length" $ do
       indexAfterUniquePrefix 4 "bvwbjplbgvbhsrlpgdmjqwftvncz" `shouldBe` 5
       indexAfterUniquePrefix 4 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" `shouldBe` 10
+      indexAfterUniquePrefix 4 "aabcd" `shouldBe` 5
       indexAfterUniquePrefix 14 "bvwbjplbgvbhsrlpgdmjqwftvncz" `shouldBe` 23
       indexAfterUniquePrefix 14 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" `shouldBe` 29
+
+    it "Returns -1 if there is no unique prefix" $ do
+      indexAfterUniquePrefix 4 "abcabca" `shouldBe` -1
+      indexAfterUniquePrefix 4 "abdd" `shouldBe` -1
+
+    it "Returns -1 if the string is shorter than the prefix length" $ do
+      indexAfterUniquePrefix 4 "a" `shouldBe` -1
 
   describe "parseLines" $ do
     it "Gives the first line of the input" $ do
