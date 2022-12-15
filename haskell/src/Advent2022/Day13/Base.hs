7b09f97b3ef18b0c2a32d0a97ba9cadbdf1cadc1
Gary Bryan <gary.bryan@lloydsbanking.com>
2022-12-15 17:13:35 +0000

    refactor: Day 13: use comparison directly on packets

--- haskell/src/Advent2022/Day13/Base.hs
+++ haskell/src/Advent2022/Day13/Base.hs
@@ -2,7 +2,7 @@ module Advent2022.Day13.Base
   ( Packet (..),
     Pair,
     parsePacket,
-    isInOrder,
+    comparePackets,
   )
 where
 
@@ -31,23 +31,17 @@ parsePacket s = case parse (packetParser <* eof) "" s of
   Left err -> error $ "parse error: " ++ show err
   Right p -> p
 
-isInOrder :: Pair -> Maybe Bool
-isInOrder (PInt l, PInt r) = case compare l r of
-  LT -> Just True
-  GT -> Just False
-  EQ -> Nothing
-isInOrder (p, PInt i) = isInOrder (p, PPacket [PInt i])
-isInOrder (PInt i, p) = isInOrder (PPacket [PInt i], p)
-isInOrder (PPacket [], PPacket []) = Nothing
-isInOrder (PPacket [], _) = Just True
-isInOrder (_, PPacket []) = Just False
-isInOrder (PPacket (lp : lps), PPacket (rp : rps)) = case isInOrder (lp, rp) of
-  Just b -> Just b
-  Nothing -> isInOrder (PPacket lps, PPacket rps)
+comparePackets :: Packet -> Packet -> Ordering
+comparePackets (PInt l) (PInt r) = compare l r
+comparePackets p (PInt i) = comparePackets p (PPacket [PInt i])
+comparePackets (PInt i) p = comparePackets (PPacket [PInt i]) p
+comparePackets (PPacket []) (PPacket []) = EQ
+comparePackets (PPacket []) _ = LT
+comparePackets _ (PPacket []) = GT
+comparePackets (PPacket (lp : lps)) (PPacket (rp : rps)) = case comparePackets lp rp of
+  EQ -> comparePackets (PPacket lps) (PPacket rps)
+  result -> result
 
--- Sort packets by comparing with isInOrder. There might be a more efficient way?
+-- Sort packets by comparing with comparePackets. There might be a more efficient way?
 instance Ord Packet where
-  compare p1 p2 = case isInOrder (p1, p2) of
-    Just True -> LT
-    Just False -> GT
-    Nothing -> EQ
+  compare = comparePackets
--- haskell/test/Advent2022/Day13/BaseSpec.hs
+++ haskell/test/Advent2022/Day13/BaseSpec.hs
@@ -36,47 +36,47 @@ spec = do
     it "parses a packet containing numbers and lists" $ do
       parsePacket "[[1],5,[2,3,[54]]]" `shouldBe` PPacket [PPacket [PInt 1], PInt 5, PPacket [PInt 2, PInt 3, PPacket [PInt 54]]]
 
-  describe "isInOrder" $ do
+  describe "comparePackets" $ do
     describe "compare two integers" $ do
-      it "is true if the left integer is lower" $ do
-        isInOrder (PInt 4, PInt 8) `shouldBe` Just True
+      it "is LT if the left integer is lower" $ do
+        comparePackets (PInt 4) (PInt 8) `shouldBe` LT
 
-      it "is false if the left integer is higher" $ do
-        isInOrder (PInt 77, PInt 6) `shouldBe` Just False
+      it "is GT if the left integer is higher" $ do
+        comparePackets (PInt 77) (PInt 6) `shouldBe` GT
 
       it "indicates if the integers are equal" $ do
-        isInOrder (PInt 6, PInt 6) `shouldBe` Nothing
+        comparePackets (PInt 6) (PInt 6) `shouldBe` EQ
 
     describe "compare integers to lists" $ do
-      it "is true if the left integer is lower" $ do
-        isInOrder (PPacket [PInt 4], PInt 8) `shouldBe` Just True
+      it "is LT if the left integer is lower" $ do
+        comparePackets (PPacket [PInt 4]) (PInt 8) `shouldBe` LT
 
-      it "is false if the left integer is higher" $ do
-        isInOrder (PInt 77, PPacket [PInt 6]) `shouldBe` Just False
+      it "is GT if the left integer is higher" $ do
+        comparePackets (PInt 77) (PPacket [PInt 6]) `shouldBe` GT
 
     describe "compare lists of integers" $ do
-      it "is true if the first left integer is lower than the first right" $ do
-        isInOrder (PPacket [PInt 4, PInt 20], PPacket [PInt 8, PInt 2]) `shouldBe` Just True
+      it "is LT if the first left integer is lower than the first right" $ do
+        comparePackets (PPacket [PInt 4, PInt 20]) (PPacket [PInt 8, PInt 2]) `shouldBe` LT
 
-      it "is false if the first left integer is higher than the first right" $ do
-        isInOrder (PPacket [PInt 77, PInt 2], PPacket [PInt 6, PInt 5]) `shouldBe` Just False
+      it "is GT if the first left integer is higher than the first right" $ do
+        comparePackets (PPacket [PInt 77, PInt 2]) (PPacket [PInt 6, PInt 5]) `shouldBe` GT
 
-      it "is true if the left list runs out of items first" $ do
-        isInOrder (PPacket [PInt 4, PInt 10], PPacket [PInt 4, PInt 10, PInt 20]) `shouldBe` Just True
+      it "is LT if the left list runs out of items first" $ do
+        comparePackets (PPacket [PInt 4, PInt 10]) (PPacket [PInt 4, PInt 10, PInt 20]) `shouldBe` LT
 
-      it "is false if the right list runs out of items first" $ do
-        isInOrder (PPacket [PInt 4, PInt 10, PInt 20], PPacket [PInt 4, PInt 10]) `shouldBe` Just False
-        isInOrder (PPacket [PInt 7, PInt 7, PInt 7], PPacket [PInt 7, PInt 7]) `shouldBe` Just False
+      it "is GT if the right list runs out of items first" $ do
+        comparePackets (PPacket [PInt 4, PInt 10, PInt 20]) (PPacket [PInt 4, PInt 10]) `shouldBe` GT
+        comparePackets (PPacket [PInt 7, PInt 7, PInt 7]) (PPacket [PInt 7, PInt 7]) `shouldBe` GT
 
       it "is equal if both lists run out of items" $ do
-        isInOrder (PPacket [PInt 4, PInt 10], PPacket [PInt 4, PInt 10]) `shouldBe` Nothing
+        comparePackets (PPacket [PInt 4, PInt 10]) (PPacket [PInt 4, PInt 10]) `shouldBe` EQ
 
   describe "compare mixed lists" $ do
     it "compares an integer to a list" $ do
-      isInOrder (PPacket [PInt 9], PPacket [PPacket [PInt 8, PInt 7]]) `shouldBe` Just False
+      comparePackets (PPacket [PInt 9]) (PPacket [PPacket [PInt 8, PInt 7]]) `shouldBe` GT
 
     it "compares a list of integers to a list of lists of integers" $ do
-      isInOrder (PPacket [PInt 4, PInt 10], PPacket [PPacket [PInt 4, PInt 20], PInt 5]) `shouldBe` Just True
+      comparePackets (PPacket [PInt 4, PInt 10]) (PPacket [PPacket [PInt 4, PInt 20], PInt 5]) `shouldBe` LT
 
     it "compares a list of integers to a list of a list then integers" $ do
-      isInOrder (PPacket [PInt 4, PInt 10], PPacket [PPacket [PInt 4], PInt 20, PInt 5]) `shouldBe` Just True
+      comparePackets (PPacket [PInt 4, PInt 10]) (PPacket [PPacket [PInt 4], PInt 20, PInt 5]) `shouldBe` LT
