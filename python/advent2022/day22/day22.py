71210fc426817ccaa34d5903040aac0f0784abc7
Gary Bryan <garybryan@gmail.com>
2023-03-23 19:32:04 +0000

    Basic map parsing.
    Might need to be smarter to handle skipping over blank entries more efficiently.

--- python/advent2022/day22/day22.py
+++ python/advent2022/day22/day22.py
@@ -1,12 +1,14 @@
 import re
-from typing import Literal
+from typing import Literal, cast
 
 Direction = Literal["R", "L"]
 Move = str | int
 
+Tile = Literal[".", "#"] | None
 
-def parse_path(path: str) -> list[Move]:
-    parts = re.findall(r"(\d+|[RL])", path)
+
+def parse_path(path_text: str) -> list[Move]:
+    parts = re.findall(r"(\d+|[RL])", path_text)
 
     def parse_part(part: str) -> Move:
         if part.isnumeric():
@@ -14,3 +16,19 @@ def parse_path(path: str) -> list[Move]:
         return part
 
     return list(map(parse_part, parts))
+
+
+def parse_map(map_lines: list[str]) -> list[list[Tile]]:
+    def parse_char(char: str) -> Tile:
+        if char == " ":
+            return None
+
+        if char in (".", "#"):
+            return char
+
+        raise ValueError(f"Invalid map character: {char}")
+
+    def parse_line(line: str) -> list[Tile]:
+        return cast(list[Tile], list(map(parse_char, line)))
+
+    return list(map(parse_line, map_lines))
